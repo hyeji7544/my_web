806cb7dac4563cab8993b7ad0dd9ee1425030065
Hyeji.Kim <hyeji.kim@qrious.co.nz>
2020-05-27 19:39:07 +1200

    habit page changed

--- yr2020.js
+++ yr2020.js
@@ -1,6 +1,3 @@
 function myf() {
-	var textInput = document.getElementById("book").value;
-	document.getElementById("bookInput").innerHTML = textInput
-}
 
-/* document.querySelector("body").style.backgroundColor = "honeydew"; */
+}
